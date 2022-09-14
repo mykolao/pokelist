d1552bb2f57b58a710fc32b5e1156ca815937553
mykolao <mykolaoleskiv@gmail.com>
2022-09-14 23:16:54 +0300

    feat: Footer

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,4 @@
+import Footer from 'components/Footer';
 import Header from 'components/Header';
 import Main from 'components/Main';
 
@@ -5,6 +6,7 @@ const App = () => (
   <div>
     <Header />
     <Main />
+    <Footer />
   </div>
 );
 
