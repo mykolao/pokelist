a62a75bb2f9d852ed63e4778d474018e8f12c1e3
mykolao <mykolaoleskiv@gmail.com>
2022-09-15 08:32:18 +0300

    style: App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,10 @@
+import styles from 'components/App/App.module.scss';
 import Footer from 'components/Footer';
 import Header from 'components/Header';
 import Main from 'components/Main';
 
 const App = () => (
-  <div>
+  <div className={styles.app}>
     <Header />
     <Main />
     <Footer />
