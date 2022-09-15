1d1cc067221ba02e16a01b6809d9fe3fb9dbed09
mykolao <mykolaoleskiv@gmail.com>
2022-09-15 12:44:07 +0300

    style: Card and CardList

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 
 import { useGetPokemonByNameQuery } from 'api';
+import styles from 'components/Card/Card.module.scss';
 import CardImage from 'components/CardImage';
 
 interface Props {
@@ -15,11 +16,29 @@ export const Card: FC<Props> = ({ name }) => {
   }
 
   const { sprites } = pokemon!;
+  const cardName = pokemon!.name.split('-').join(' ');
+
+  if (!sprites.front_default) {
+    return (
+      <li className={styles.static}>
+        <div className={styles.front}>
+          <CardImage sprite={sprites.front_default} />
+          <p className={styles.name}>{cardName}</p>
+        </div>
+      </li>
+    );
+  }
 
   return (
-    <li>
-      <CardImage name={name} sprites={sprites} />
-      <p>{pokemon!.name}</p>
+    <li className={styles.flippable}>
+      <div className={styles.front}>
+        <CardImage sprite={sprites.front_default} />
+        <p className={styles.name}>{cardName}</p>
+      </div>
+      <div className={styles.back}>
+        <CardImage sprite={sprites.back_default} />
+        <p className={styles.name}>{cardName}</p>
+      </div>
     </li>
   );
 };
