27d5ada42f43ac428fa3bfd0e17ffefeb76f268f
mykolao <mykolaoleskiv@gmail.com>
2022-09-15 12:43:30 +0300

    remove: hover effect

--- src/components/CardImage/CardImage.tsx
+++ src/components/CardImage/CardImage.tsx
@@ -1,40 +1,16 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 
-import { IPokemonDetails } from 'api';
 import pokeball from 'assets/pokeball.svg';
 import styles from 'components/CardImage/CardImage.module.scss';
 
-interface Props extends Pick<IPokemonDetails, 'name' | 'sprites'> {}
+interface Props {
+  sprite: string | null;
+}
 
-export const CardImage: FC<Props> = ({ name, sprites }) => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  const front = sprites.front_default;
-  const back = sprites.back_default;
-
-  const handleMouseEnter = () => {
-    if (!isHovered) {
-      setIsHovered(true);
-    }
-  };
-
-  const handleMouseLeave = () => {
-    if (isHovered) {
-      setIsHovered(false);
-    }
-  };
-
-  if (!front) {
+export const CardImage: FC<Props> = ({ sprite }) => {
+  if (!sprite) {
     return <img className={styles.image} src={pokeball} alt="unknown pokemon" />;
   }
 
-  return (
-    <img
-      className={styles.image}
-      src={isHovered ? back : front}
-      alt={name}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    />
-  );
+  return <img className={styles.image} src={sprite} alt="pokemon" />;
 };
